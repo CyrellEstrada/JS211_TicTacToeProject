5b337c7af3028d02d217ec65bb7afb55250697ba
CyrellEstrada <cyrellestrada1@gmail.com>
2024-06-17 18:45:59 -0500

    TicTacToe_Homework

--- main.js
+++ main.js
@@ -22,6 +22,14 @@ let board = [
 // using let because the variable is expected to change from 'X' to 'O' and back
 let playerTurn = 'X';
 
+const changeTurn = () => {
+  if(playerTurn === "X") {
+    playerTurn = "O"
+  } else {
+    playerTurn = "X"
+  }
+}
+
 // is a function that print the current status of the board using the variable - board
 const printBoard = () => {
   console.log('   0  1  2');
@@ -32,25 +40,78 @@ const printBoard = () => {
   console.log('2 ' + board[2].join(' | '));
 }
 
+// let board = [
+//   [' ', ' ', ' '],
+//   [' ', ' ', ' '],
+//   [' ', ' ', ' ']
+// ];
+
 const horizontalWin = () => {
   // Your code here to check for horizontal wins
+for (let i= 0; i < board.length; i++) {
+  if (board[i][0] !== " " && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+    return true
+  }
 }
+return false
+}
+
 
 const verticalWin = () => {
   // Your code here to check for vertical wins
+  for (let col = 0; col < board[0].length; col++) {
+    let allSame = true
+
+    for (let row = 0; row < board.length; row++) {
+      if (board[row][col] === " " || board[row][col] !== board[0][col]) {
+        allSame = false;
+        break;
+      }
+    }
+
+    if (allSame) {
+      return true;
+    }
+  }
+  return false;
 }
 
 const diagonalWin = () => {
   // Your code here to check for diagonal wins
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][i] !== " " & board[i][i] === board[0][0]) {
+      return true;
+    }
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][board.length - 1 - i] !== " " && board[i][board.length - 1 - i] === board[0][board.length -1]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 const checkForWin = () => {
   // Your code here call each of the check for types of wins
+  return horizontalWin() || verticalWin() || diagonalWin() ;
 }
 
 const ticTacToe = (row, column) => {
   // Your code here to place a marker on the board
+  if (row < 0 || row >= board.length || column < 0 || column >= board[0].length) {
+    console.log("Invalid move.");
+    return;
+  }
+  if (board[row][column] !== " ") {
+    console.log("Cell is occupied, please choose another.")
+  }
+  board[row][column] = playerTurn;
   // then check for a win
+  if (checkForWin()) {
+    console.log("Player " + playerTurn + " wins!");
+  }
+  changeTurn();
 }
 
 const getPrompt = () => {
@@ -62,6 +123,7 @@ const getPrompt = () => {
       getPrompt();
     });
   });
+  // changeTurn()
 }
 
 
